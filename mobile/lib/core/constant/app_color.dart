521ab8497389fce79edd0ecc4e244038899418d2
Gylmynnn <muhamadgilman999@gmail.com>
2025-07-07 20:14:52 +0800

    core : app color
    add app everblush color theme

---
+++ mobile/lib/core/constant/app_color.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const background = Color(0xFF141B1E);
+  static const foreground = Color(0xFFDADADA);
+  static const red = Color(0xFFE57474);
+  static const green = Color(0xFF8CCF7E);
+  static const yellow = Color(0xFFE5C76B);
+  static const blue = Color(0xFF67B0E8);
+  static const magenta = Color(0xFFC47FD5);
+  static const cyan = Color(0xFF6CBFBF);
+  static const orange = Color(0xFFF57D26);
+  static const black = Color(0xff232a2d);
+}
