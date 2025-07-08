42e9c5ebf0a9020dc739b0e2ae07aea57d899e5d
Gylmynnn <muhamadgilman999@gmail.com>
2025-07-09 02:31:50 +0800

    feat : homepage

---
+++ website/src/routes/homepage/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import { enhance } from "$app/forms";
+</script>
+
+<span>home</span>
+<form action="?/logout" method="POST" use:enhance>
+    <button type="submit" name="logout">Logout</button>
+</form>
